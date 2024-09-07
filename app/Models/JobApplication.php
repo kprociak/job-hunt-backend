431c1c1f374f84551dd079c22dd39178e463234e
Krzysztof Prociak <krzysztof@prociak.pl>
2024-09-07 14:09:09 +0200

    last update date added to job application

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -24,6 +24,24 @@ class JobApplication extends Model
         'status',
     ];
 
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'company_name' => $this->company_name,
+            'job_title' => $this->job_title,
+            'offer_url' => $this->offer_url,
+            'offer_salary_from' => $this->offer_salary_from,
+            'offer_salary_to' => $this->offer_salary_to,
+            'expected_salary_from' => $this->expected_salary_from,
+            'expected_salary_to' => $this->expected_salary_to,
+            'application_date' => $this->application_date,
+            'notes' => $this->notes,
+            'status' => $this->status,
+            'last_update_date' => $this->lastUpdateDate(),
+        ];
+    }
+
     public function recruitmentEvents(): HasMany
     {
         return $this->hasMany(RecruitmentEvent::class);
@@ -32,6 +50,14 @@ class JobApplication extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function lastUpdateDate(): string
+    {
+        if ($this->recruitmentEvents->count() > 0) {
+            return $this->recruitmentEvents->sortByDesc('date')->first()->date;
+        }
+        return $this->application_date;
     }
 
 }
